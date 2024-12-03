ec9854e686f9f11e9b49330782e15d7516df8b40
Liu Xiang <liuxiang921@gmail.com>
2024-12-03 21:28:56 +0800

    Initial commit

---
+++ lib/md_block_converter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "md_block_converter/version"
+
+module MdBlockConverter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/md_block_converter/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MdBlockConverter
+  VERSION = "0.1.0"
+end
